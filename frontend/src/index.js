4b0c185b46cf2f8e4760f3ba48c6699bd6580695
lopessoftec <lopessoftec@gmail.com>
2020-09-22 21:53:31 -0300

    finalizado nivel 1

---
+++ frontend/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './App';
+
+// JSX: html dentro do javascript (javascript xml)
+
+render(<App />, document.getElementById('app'));
